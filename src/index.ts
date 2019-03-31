de8615e37ad75195b47743c2e90ac75b6bcac7e8
Peter Zhou <peter.zhou@yale.edu>
2019-03-30 21:40:53 -0400

    expose langprops

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as withTranslation } from "./withTranslation";
 export { default as withLang } from "./withLang";
 export { default as useLang } from "./useLang";
+export { LangProps } from "./types";
